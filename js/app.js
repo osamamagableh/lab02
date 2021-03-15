42c3cb9ac25005146b28950808dbcb9f8de00f88
osamamagableh <osama.magableh@gmail.com>
2021-03-15 13:46:55 +0200

    Add new file

---
+++ js/app.js
@@ -0,0 +1,52 @@
+'use strict';
+let n=prompt('please Enter your name ?')
+alert('Welcom '+n+' in my webpage');
+alert('please answer the following qustions with yes/no  OR  y/n');
+let qus1 = prompt('Am I from Jordan ?').toLowerCase();
+if(qus1=='yes' || qus1=='y'){
+    console.log('yes is a correct answer');
+    alert('yes is a correct answer');
+}
+else
+{
+    alert('your answer is incorrect')
+}
+let qus2 = prompt('Do I like pizza ?').toLowerCase();
+if(qus2=='yes'|| qus2=='y'){
+    console.log('yes is a correct answer');
+    alert('yes is a correct answer');
+}
+else
+{
+    alert('your answer is incorrect')
+}
+let qus3 = prompt('Do I have a car ?').toLowerCase();
+if(qus3=='no'|| qus3=='n'){
+    console.log('no is a correct answer');
+    alert('no is a correct answer');
+}
+else
+{
+    alert('your answer is incorrect')
+}
+let qus4 = prompt('Do I like playing card ?').toLowerCase();
+if(qus4=='yes'|| qus4=='y'){
+    console.log('yes is a correct answer');
+    alert('yes is a correct answer');
+}
+else
+{
+    alert('your answer is incorrect')
+}
+let qus5 = prompt('Do I like parsley ?').toLowerCase();
+if(qus5=='no'|| qus5=='n'){
+    console.log('no is a correct answer');
+    alert('no is a correct answer');
+}
+else
+{
+    alert('your answer is incorrect')
+}
+
+
+
